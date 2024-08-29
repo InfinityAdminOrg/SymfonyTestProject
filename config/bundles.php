4f9696d673d7d0b68fe15be67020fa482881e265
pkly <kakolp95@gmail.com>
2024-08-29 17:06:52 +0200

    Fix routes, logging in

--- config/bundles.php
+++ config/bundles.php
@@ -11,5 +11,5 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
-    InfinityBundle\InfinityBundle::class => ['all' => true],
+    Infinity\InfinityBundle::class => ['all' => true],
 ];
